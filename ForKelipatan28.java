787c85abe5cb1a723adc6b9b1ebbeb25a1be4464
TyaraKusumaNevada <tyaranevada29@gmail.com>
2023-10-10 21:23:14 +0700

    modifikasi kode program

--- ForKelipatan28.java
+++ ForKelipatan28.java
@@ -5,7 +5,7 @@ public class ForKelipatan28 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in); 
         int  kelipatan,jumlah=0, counter=0;
-        System.out.println(" Masukkan bilangan kelipatan (1-9):");
+        System.out.println("Masukkan bilangan kelipatan (1-9):");
         kelipatan = scan.nextInt();
         for (int i = 1; i<= 50; i++) {
             if ( i % kelipatan == 0 ) {
@@ -13,8 +13,10 @@ public class ForKelipatan28 {
                 counter++;
             }
         }
+        double rataRata= (double) jumlah/ counter;
         System.out.printf(" Banyak bilangan %d dari 1 sampai 50 adalah %d\n",kelipatan, counter);
         System.out.printf(" Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf(" Rata - rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
         
     }
 }
